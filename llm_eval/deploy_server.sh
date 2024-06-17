25aef82c9eb272bd05b2f59c0a56535e04365204
Tobias Brugger <tobias.brugger@bfh.ch>
2024-06-17 11:55:01 +0200

    bugfix

--- llm_eval/deploy_server.sh
+++ llm_eval/deploy_server.sh
@@ -4,12 +4,11 @@ source settings.env
 curl --location 'https://marketplace.tensordock.com/api/v0/client/deploy/single' \
 --data-urlencode "api_key=$API_KEY" \
 --data-urlencode "api_token=$API_TOKEN" \
---data-urlencode "password=$superC0mplexPassword" \
 --data-urlencode "name=$NAME" \
 --data-urlencode "gpu_count=$GPU_COUNT" \
 --data-urlencode "gpu_model=$GPU_MODEL" \
---data-urlencode "vcpus=$16" \
---data-urlencode "ram=$128" \
+--data-urlencode "vcpus=$VCPUS" \
+--data-urlencode "ram=$RAM" \
 --data-urlencode "external_ports=$EXTERNAL_PORTS" \
 --data-urlencode "internal_ports=$INTERNAL_PORTS" \
 --data-urlencode "hostnode=$HOSTNODE" \
