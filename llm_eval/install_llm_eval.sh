02024f670cd66a550f8733c5d7967513a870419b
Joel Niklaus <me@joelniklaus.ch>
2024-07-01 13:35:57 +0200

    updated installation script

--- llm_eval/install_llm_eval.sh
+++ llm_eval/install_llm_eval.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 activate () {
     echo "Activating virtual environment..."
     . .venv/bin/activate
@@ -5,13 +7,11 @@ activate () {
 
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y python3 python3-pip python3-venv git
-sudo apt install ninja-build # for Flash attention
-mkdir ~/llm_eval
+mkdir -p ~/llm_eval
 cd ~/llm_eval
 python3 -m venv .venv
 activate
-pip install --upgrade pip setuptools wheel
-pip install packaging
-pip install git+https://github.com/Dao-AILab/flash-attention.git
+pip install --upgrade pip setuptools wheel cuda-python torch transformers datasets
+pip install pytest triton einops tiktoken sentencepiece huggingface_hub[cli] accelerate protobuf flash-attn
 pip install git+https://github.com/EleutherAI/lm-evaluation-harness.git
 mkdir output
