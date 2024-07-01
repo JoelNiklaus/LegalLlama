c0e751b456b18666fd5ad695bb985ab589aaf872
Joel Niklaus <me@joelniklaus.ch>
2024-07-01 18:25:25 +0200

    improved evaluation script

--- llm_eval/llm_eval.sh
+++ llm_eval/llm_eval.sh
@@ -9,14 +9,23 @@ activate
 
 # Inspired by the Euro-LLM-Leaderboard: https://huggingface.co/spaces/occiglot/euro-llm-leaderboard
 
+TASKS=(
+    "TruthfulQA"
+    "Belebele"
+    "ARC"
+    "MMLU"
+) # Exclude "HellaSwag" for cost reasons
+
 MODELS=(
-    #"Qwen/Qwen2-7B-Instruct"
-    #"Qwen/Qwen2-1.5B-Instruct"
-    #"Qwen/Qwen2-0.5B-Instruct"
-    #"microsoft/Phi-3-medium-128k-instruct" # now evaluated on the leaderboard
-    "google/gemma-2-9b-it"
-    "microsoft/Phi-3-small-128k-instruct"
-    "microsoft/Phi-3-mini-128k-instruct" # CUDA memory error for some reason
+    #"Qwen/Qwen2-7B-Instruct" # completed
+    #"Qwen/Qwen2-1.5B-Instruct" # completed
+    #"Qwen/Qwen2-0.5B-Instruct" # completed
+    #"mistralai/Mistral-7B-Instruct-v0.3" # completed
+    #"microsoft/Phi-3-medium-128k-instruct" # completed
+    #"microsoft/Phi-3-small-128k-instruct" # completed: Belebele needs constant batch size 16
+    "microsoft/Phi-3-mini-128k-instruct" # Belebele, ARC, and MMLU fail with OOM
+    #"CohereForAI/aya-23-8B" # completed
+    #"google/gemma-2-9b-it" # completed: ARC needs constant batch size 8
 )
 OUTPUT_DIR="./output"
 
@@ -30,8 +39,12 @@ run_lm_eval() {
     local num_fewshot=$3
     local task_name=$4
 
+    BATCH_SIZE="8"
+    BATCH_SIZE="auto:4"
+
     echo "Running $task_name with $num_fewshot-shot for model $model"
-    if ! lm_eval --model hf --model_args pretrained=$model --tasks $task_list --num_fewshot $num_fewshot --batch_size auto:4 --output_path "${OUTPUT_DIR}" --trust_remote_code; then
+    # IMPORTANT: Remove "accelerate launch --no-python" to run on only one GPU
+    if ! accelerate launch --no-python lm_eval --model hf --model_args pretrained=$model --tasks $task_list --num_fewshot $num_fewshot --batch_size $BATCH_SIZE --output_path "${OUTPUT_DIR}" --trust_remote_code; then
         echo "The evaluation for $task_name with model $model failed. Please check the logs in ${OUTPUT_DIR} for more information."
         return 1
     fi
@@ -43,21 +56,30 @@ run_lm_eval() {
 for MODEL in "${MODELS[@]}"; do
     echo "Starting evaluations for model: $MODEL"
 
-    # TruthfulQA: approx. 3m on A100 for Phi-3-medium-128k-instruct
-    run_lm_eval "$MODEL" "truthfulqa_mc2,truthfulqa_de_mc2,truthfulqa_fr_mc2,truthfulqa_es_mc2,truthfulqa_it_mc2" 0 "TruthfulQA" || continue
+    if [[ " ${TASKS[*]} " =~ " TruthfulQA " ]]; then
+        # TruthfulQA: approx. 3m on A100 for Phi-3-medium-128k-instruct
+        run_lm_eval "$MODEL" "truthfulqa_mc2,truthfulqa_de_mc2,truthfulqa_fr_mc2,truthfulqa_es_mc2,truthfulqa_it_mc2" 0 "TruthfulQA" || continue
+    fi
 
-    # Belebele: approx. 5m on A100 for Phi-3-medium-128k-instruct
-    run_lm_eval "$MODEL" "belebele_eng_Latn,belebele_ita_Latn,belebele_deu_Latn,belebele_fra_Latn,belebele_spa_Latn" 5 "Belebele" || continue
+    if [[ " ${TASKS[*]} " =~ " Belebele " ]]; then
+        # Belebele: approx. 5m on A100 for Phi-3-medium-128k-instruct
+        run_lm_eval "$MODEL" "belebele_eng_Latn,belebele_ita_Latn,belebele_deu_Latn,belebele_fra_Latn,belebele_spa_Latn" 5 "Belebele" || continue
+    fi
 
-    # ARC: approx. 20m on A100 for Phi-3-medium-128k-instruct
-    run_lm_eval "$MODEL" "arc_challenge,arc_de,arc_es,arc_it,arc_fr" 25 "ARC" || continue
+    if [[ " ${TASKS[*]} " =~ " ARC " ]]; then
+        # ARC: approx. 20m on A100 for Phi-3-medium-128k-instruct
+        run_lm_eval "$MODEL" "arc_challenge,arc_de,arc_es,arc_it,arc_fr" 25 "ARC" || continue
+    fi
 
-    # MMLU: approx. 1h on A100 for Phi-3-medium-128k-instruct
-    run_lm_eval "$MODEL" "m_mmlu_en,m_mmlu_de,m_mmlu_fr,m_mmlu_es,m_mmlu_it" 5 "MMLU" || continue
+    if [[ " ${TASKS[*]} " =~ " MMLU " ]]; then
+        # MMLU: approx. 1h on A100 for Phi-3-medium-128k-instruct
+        run_lm_eval "$MODEL" "m_mmlu_en,m_mmlu_de,m_mmlu_fr,m_mmlu_es,m_mmlu_it" 5 "MMLU" || continue
+    fi
 
-    # HellaSwag: approx. 2.5h on A100 for Phi-3-medium-128k-instruct
-    # (16h for all) ==> skip for cost reasons
-    # run_lm_eval "$MODEL" "hellaswag,hellaswag_es,hellaswag_it,hellaswag_fr,hellaswag_de" 10 "HellaSwag" || continue
+    if [[ " ${TASKS[*]} " =~ " HellaSwag " ]]; then
+        # HellaSwag: approx. 2.5h on A100 for Phi-3-medium-128k-instruct (16h for all) ==> skip for cost reasons
+        run_lm_eval "$MODEL" "hellaswag,hellaswag_es,hellaswag_it,hellaswag_fr,hellaswag_de" 10 "HellaSwag" || continue
+    fi
 
     # Possible additional supported benchmark: lambada_openai_mt_en,lambada_openai_mt_de,lambada_openai_mt_fr,lambada_openai_mt_es,lambada_openai_mt_it
 
