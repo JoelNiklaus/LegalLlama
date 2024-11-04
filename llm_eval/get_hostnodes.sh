3844ad30eba9c58668dc48d93d8d3b1e70ab8a16
Joel Niklaus <me@joelniklaus.ch>
2024-11-04 11:44:29 +0100

    Removed obsolete files.

--- llm_eval/get_hostnodes.sh
+++
@@ -1,8 +0,0 @@
-source secrets.env
-source settings.env
-curl --location --request GET 'https://marketplace.tensordock.com/api/v0/client/deploy/hostnodes' \
---data-urlencode "minRAM=$RAM" \
---data-urlencode "minVCPUs=$VCPUS" \
---data-urlencode "minGPUs=$GPU_COUNT" \
---data-urlencode "minStorage=$STORAGE" \
-| jq '.hostnodes | to_entries[] | {hostnode_id: .key, gpu: .value.specs.gpu, status: .value.status}' | grep -B 2 -A 15 $GPU_MODEL
