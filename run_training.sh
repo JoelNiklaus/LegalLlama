ce4cc0133a6b9330c2f10b9a6481d8eb9e5d12f6
Joel Niklaus <joel@harvey.ai>
2024-12-10 08:46:45 +0000

    Updated training progress.

--- run_training.sh
+++ run_training.sh
@@ -10,28 +10,15 @@ set +a
 
 # See available models at https://huggingface.co/unsloth
 
-# Batch sizes on an 80GB H100 with max_seq_length=2048:
-# unsloth/Phi-3.5-mini-instruct-bnb-4bit: 128 uses 59GB VRAM
-# unsloth/Qwen2.5-7B-Instruct-bnb-4bit: 32 uses 66GB VRAM
-# unsloth/Qwen2.5-14B-Instruct-bnb-4bit: 32 uses 72GB VRAM
-# unsloth/gemma-2-27b-bnb-4bit: 16 uses 67GB VRAM
+# For some reason, gemma-2-27b-it yields NaN in the grad_norm, so we don't use it.
+# MODELS=("google/madlad400-10b-mt" "facebook/seamless-m4t-v2-large") # These are not yet supported by unsloth
 
-# Batch sizes on an 80GB H100 with max_seq_length=512:
-# Llama-3.2-1B-Instruct: 128 uses 51GB VRAM
-# Qwen2.5-7B-Instruct: 128 uses 66GB VRAM
-# Qwen2.5-14B-Instruct: 128 uses 74GB VRAM,
-# Qwen2.5-0.5B-Instruct: 128 uses 59GB VRAM, 64 uses 30GB VRAM
-
-# GPU 0: running 32B Qwen2.5-Instruct then gemma-2-27b-it then gemma-2-2b-it then gemma-2-9b-it
-# GPU 1: running 70B Llama-3.1-Instruct
-
-MODELS=("Qwen2.5-32B-Instruct" "gemma-2-27b-it" "gemma-2-2b-it" "gemma-2-9b-it")
-MODELS=("Meta-Llama-3.1-70B-Instruct")
+MODELS=()
     
-MODELS_DONE=("Llama-3.2-1B-Instruct" "Llama-3.2-3B-Instruct" "Meta-Llama-3.1-8B-Instruct" "Phi-3.5-mini-instruct" "Phi-3-medium-4k-instruct" "mistral-7b-instruct-v0.3" "Qwen2.5-72B-Instruct" "Qwen2.5-14B-Instruct" "Qwen2.5-7B-Instruct" "Qwen2.5-3B-Instruct" "Qwen2.5-1.5B-Instruct" "Qwen2.5-0.5B-Instruct")
+MODELS_DONE=("Unbabel/TowerInstruct-13B-v0.1" "Unbabel/TowerInstruct-7B-v0.2" "gemma-2-2b-it" "gemma-2-9b-it" "Llama-3.2-1B-Instruct" "Llama-3.2-3B-Instruct" "Meta-Llama-3.1-8B-Instruct" "Meta-Llama-3.1-70B-Instruct" "Phi-3.5-mini-instruct" "Phi-3-medium-4k-instruct" "mistral-7b-instruct-v0.3" "Qwen2.5-72B-Instruct" "Qwen2.5-32B-Instruct" "Qwen2.5-14B-Instruct" "Qwen2.5-7B-Instruct" "Qwen2.5-3B-Instruct" "Qwen2.5-1.5B-Instruct" "Qwen2.5-0.5B-Instruct")
 
 for model in "${MODELS[@]}"; do
-    CUDA_VISIBLE_DEVICES=1 python3 train.py --model_name $model --batch_size 128 --learning_rate 1e-4 --lora_rank 16 --num_epochs 5 --cpus 30 --push_to_hub
+    CUDA_VISIBLE_DEVICES=0 python3 train.py --model_name $model --batch_size 128 --learning_rate 1e-4 --lora_rank 16 --num_epochs 5 --cpus 30 --push_to_hub
 done
 
 
