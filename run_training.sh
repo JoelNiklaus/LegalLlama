49cba8f5dd636951bc4ff1eac1fc660299351f98
Joel Niklaus <joel@harvey.ai>
2025-01-11 18:32:36 +0000

    Trained phi-4

--- run_training.sh
+++ run_training.sh
@@ -17,9 +17,9 @@ set +a
 # The other model trains, but then at inference has errors
 # Cohere is not supported by unsloth yet: https://huggingface.co/unsloth/c4ai-command-r-plus-08-2024-bnb-4bit/discussions/1
 # For some reason, the loss is extremely high. Check again and abort if it is not fixed.
-MODELS=("c4ai-command-r-plus-08-2024" "c4ai-command-r-08-2024")
-    
-MODELS_DONE=("Unbabel/TowerInstruct-13B-v0.1" "Unbabel/TowerInstruct-7B-v0.2" "gemma-2-2b-it" "gemma-2-9b-it" "Llama-3.2-1B-Instruct" "Llama-3.2-3B-Instruct" "Meta-Llama-3.1-8B-Instruct" "Meta-Llama-3.1-70B-Instruct" "Llama-3.3-70B-Instruct" "Phi-3.5-mini-instruct" "Phi-3-medium-4k-instruct" "mistral-7b-instruct-v0.3" "Qwen2.5-72B-Instruct" "Qwen2.5-32B-Instruct" "Qwen2.5-14B-Instruct" "Qwen2.5-7B-Instruct" "Qwen2.5-3B-Instruct" "Qwen2.5-1.5B-Instruct" "Qwen2.5-0.5B-Instruct")
+MODELS=("c4ai-command-r-08-2024" "c4ai-command-r-plus-08-2024" "gemma-2-27b-it")
+
+MODELS_DONE=("unsloth/phi-4" "Unbabel/TowerInstruct-13B-v0.1" "Unbabel/TowerInstruct-7B-v0.2" "gemma-2-2b-it" "gemma-2-9b-it" "Llama-3.2-1B-Instruct" "Llama-3.2-3B-Instruct" "Meta-Llama-3.1-8B-Instruct" "Meta-Llama-3.1-70B-Instruct" "Llama-3.3-70B-Instruct" "Phi-3.5-mini-instruct" "Phi-3-medium-4k-instruct" "mistral-7b-instruct-v0.3" "Qwen2.5-72B-Instruct" "Qwen2.5-32B-Instruct" "Qwen2.5-14B-Instruct" "Qwen2.5-7B-Instruct" "Qwen2.5-3B-Instruct" "Qwen2.5-1.5B-Instruct" "Qwen2.5-0.5B-Instruct")
 
 for model in "${MODELS[@]}"; do
     CUDA_VISIBLE_DEVICES=0 python3 train.py --model_name $model --batch_size 128 --learning_rate 1e-4 --lora_rank 16 --num_epochs 5 --cpus 30 --push_to_hub
